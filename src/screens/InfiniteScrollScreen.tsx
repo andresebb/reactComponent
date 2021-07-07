368e5e538330ba6b06afd074c98fce0211ef5a35
Andres Betancourt <aebb001@gmail.com>
2021-07-07 12:14:09 -0500

    Enviando stilos desde el padre e infinie Scroll

--- src/screens/InfiniteScrollScreen.tsx
+++ src/screens/InfiniteScrollScreen.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import {useState} from 'react';
-import {View, FlatList, Text, StyleSheet} from 'react-native';
+import {
+  View,
+  FlatList,
+  ActivityIndicator,
+  StyleSheet,
+  Image,
+} from 'react-native';
+import {FadeInImage} from '../components/FadeInImage';
 import {HeaderTitle} from '../components/HeaderTitle';
 
 export const InfiniteScrollScreen = () => {
   const [numbers, setNumbers] = useState([0, 1, 2, 3, 4, 5, 6]);
 
   const renderItem = (item: number) => {
-    return <Text style={styles.itemText}>{item}</Text>;
+    return (
+      <FadeInImage
+        uri={`https://picsum.photos/id/${item}/200/300`}
+        style={{
+          width: '100%',
+          height: 400,
+        }}
+      />
+    );
   };
 
   const loadMore = () => {
@@ -17,7 +32,9 @@ export const InfiniteScrollScreen = () => {
       newArray[i] = numbers.length + i;
     }
 
-    setNumbers([...numbers, ...newArray]);
+    setTimeout(() => {
+      setNumbers([...numbers, ...newArray]);
+    }, 1500);
   };
 
   return (
@@ -30,6 +47,18 @@ export const InfiniteScrollScreen = () => {
         onEndReached={loadMore}
         onEndReachedThreshold={0.5}
         showsVerticalScrollIndicator={false}
+        ItemSeparatorComponent={() => <View style={{height: 5}}></View>}
+        ListFooterComponent={() => (
+          <View
+            style={{
+              height: 150,
+              justifyContent: 'center',
+              width: '100%',
+              alignItems: 'center',
+            }}>
+            <ActivityIndicator size={25} color="#DDD" animating={true} />
+          </View>
+        )}
       />
     </View>
   );
@@ -37,7 +66,7 @@ export const InfiniteScrollScreen = () => {
 
 const styles = StyleSheet.create({
   itemText: {
-    backgroundColor: 'orange',
+    backgroundColor: 'blue',
     height: 150,
   },
 });
